ad8f642333eb62ab045cf80147395fc06352f1b0
Wesley Chow <wgesh8@gmail.com>
2021-01-16 10:21:04 -0800

    Fix paths, add bootstrap

--- service-worker.js
+++ service-worker.js
@@ -4,24 +4,30 @@ const cacheName = 'Offline Cache';
 const cacheAssets = [
     'index.html',
 
-    '/css/main.css',
+    './css/main.css',
+    './css/bootstrap-custom.css',
 
-    '/local/instantpage-5.1.0.js',
-    '/local/jquery.min.js',
+    './local/instantpage-5.1.0.js',
+    './local/jquery.min.js',
+    './local/bootstrap.bundle.min.js',
+    './local/bootstrap.min.css',
 
-    '/js/main.js',
-    '/js/lock.js',
+    './js/main.js',
+    './js/lock.js',
 
-    '/images/rm1-1.jpg',
-    '/images/rm1-2.jpg',
-    '/images/rm1-3.jpg',
-    '/images/rm2-1.jpg',
-    '/images/rm2-2.jpg',
-    '/images/rm2-3.jpg',
-    '/images/rm3-1.jpg',
-    '/images/rm3-2.jpg',
-    '/images/rm3-3.jpg'
+    './images/rm1-1.jpg',
+    './images/rm1-2.jpg',
+    './images/rm1-3.jpg',
+    './images/rm2-1.jpg',
+    './images/rm2-2.jpg',
+    './images/rm2-3.jpg',
+    './images/rm3-1.jpg',
+    './images/rm3-2.jpg',
+    './images/rm3-3.jpg',
 
+    './sounds/bombsound.mp3',
+    './sounds/The Bombardier\'s Song Bing Crosby.mp3',
+    './sounds/Прощанье Славянки (минус).mp3'
 ]
 
 // call install event
@@ -32,9 +38,8 @@ self.addEventListener('install', (e) => {
         caches
             .open(cacheName)
             .then(cache => {
-                // cache the files
                 console.log('Service Worker: Files Cached');
-                cache.addAll(cacheAssets).catch(r => { console.log('error')});
+                cache.addAll(cacheAssets);
             })
             .then(() => self.skipWaiting())
     );
